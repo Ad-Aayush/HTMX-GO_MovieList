770d798aeb67af286f5b7482137ba8ba1b988798
Ad-Aayush <aayushadlakha15@gmail.com>
2024-01-08 15:21:20 +0530

    Working Edit Button

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module webserver
 
 go 1.21.5
-
-require github.com/a-h/templ v0.2.513
--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var (
 	nextID = 3
 )
 
-func main() {	
+func main() {
 	// Initialize some films
 	if films["Films"] == nil {
 		films["Films"] = make(map[int]Film)
@@ -30,6 +30,7 @@ func main() {
 
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/add-film", handleAddFilm)
+	http.HandleFunc("/edit-film", handleEdit)
 	http.HandleFunc("/delete-film", handleDeleteFilm)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
@@ -68,11 +69,44 @@ func handleDeleteFilm(w http.ResponseWriter, r *http.Request) {
 		keySlice = append(keySlice, key)
 		// log.Print("Key: ", key, ", Film: ", film)
 	}
-	
+
 	sort.Ints(keySlice)
 	log.Printf("KeySlice: %v", keySlice)
 	for _, key := range keySlice {
 
 		temp.Execute(w, films["Films"][key])
+	}
+}
+
+func handleEdit(w http.ResponseWriter, r *http.Request) {
+	log.Print("Edit Film received a request.")
+	idStr := r.FormValue("filmId")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if r.Method == http.MethodPost {
+		film := films["Films"][id]
+		temp := template.Must(template.ParseFiles("edit-film.html"))
+		log.Print("POST FILM", film)
+		temp.Execute(w, film)
+	} else if r.Method == http.MethodPut {
+		log.Print("Edit Film PUT received a request.")
+
+		log.Print("ID: ", idStr)
+		title := r.FormValue("title")
+		director := r.FormValue("director")
+		film := Film{ID: id, Title: title, Director: director}
+		films["Films"][id] = film
+		log.Print("Film: ", film, idStr, id)
+		temp := template.Must(template.ParseFiles("add-film.html"))
+		temp.ExecuteTemplate(w, "edit-film", film)
+	} else if r.Method == http.MethodGet {
+		log.Print("Edit Film GET received a request.")
+
+		film := films["Films"][id]
+		temp := template.Must(template.ParseFiles("add-film.html"))
+		temp.ExecuteTemplate(w, "edit-film", film)
 	}
 }
